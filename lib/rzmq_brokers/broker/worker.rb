0a3e1e139973b753c00ec25a2f3adcc7d8e748de
Chuck Remes <cremes@mac.com>
2012-01-19 14:11:43 -0600

    a few notable fixes
    - refactor sending out requests so the responsibility is in Worker
    instead of in Broker
    
    - log error if any timer cancelation fails

--- lib/rzmq_brokers/broker/worker.rb
+++ lib/rzmq_brokers/broker/worker.rb
@@ -16,36 +16,58 @@ module RzmqBrokers
 
         @heartbeat_interval = heartbeat_interval
         @heartbeat_retries = heartbeat_retries
-        @heartbeat_timer = nil
+        @hb_sent_at = Time.at(0) # set to epoch so first heartbeat goes out
         start_heartbeat
-        @hb_received_at = Time.now
+        @hb_received_at = Time.now # true because we just received a READY msg
       end
 
       def return_address
         @envelope.map { |address| ZMQ::Message.new(address) }
       end
+      
+      def format_request(request)
+        # reset hb_sent_at so that we consider any message (including a request) to be a
+        # heartbeat; this prevents us from "overbeating" and wasting bandwidth
+        @hb_sent_at = Time.now
+        return_address + request.to_msgs
+      end
 
       def start_heartbeat
-        @reactor.log(:debug, "#{self.class}, Worker [#{@identity}] for service [#{@service_name}] starting heartbeat with interval [#{@heartbeat_interval}]")
+        @reactor.log(:debug, "#{self.class}, Worker [#{@identity}] for service [#{@service_name}] starting heartbeat with interval [#{@heartbeat_interval}] for reactor [#{@reactor.name}] on thread [#{Thread.current['reactor-name']}]")
+        @dead = false
         @heartbeat_timer = @reactor.periodical_timer(@heartbeat_interval) { beat }
       end
 
       def beat
-        @handler.send_worker_heartbeat(self)
+        now = Time.now
+        elapsed = (now - @hb_sent_at) * 1_000 # convert to milliseconds so we are comparing the same "units"
+        
+        if elapsed >= @heartbeat_interval
+          @hb_sent_at = now
+          @reactor.log(:debug, "#{self.class}, Worker [#{@identity}] for service [#{@service_name}] is sending a HB to the worker.")
+          @handler.send_worker_heartbeat(self)
+          @reactor.log(:warn, "#{self.class}, Worker [#{@identity}] is dead for service [#{@service_name}] but the broker is still sending heartbeats! Make sure this object is only ever accessed from its own reactor thread!") if @dead
+        end
       end
 
-      # Called by handler whenever it receives a worker heartbeat. 
+      # Called by handler whenever it receives a worker heartbeat.
       #
       def process_heartbeat(message = nil)
-        @reactor.log(:debug, "#{self.class}, On broker, worker [#{@identity}] received a HB message.")
+        @reactor.log(:debug, "#{self.class}, On broker, worker [#{@identity}] for service [#{@service_name}] received a HB message.")
         @hb_received_at = Time.now
       end
 
       # Called when the worker has sent a DISCONNECT or its heartbeats have timed out.
       #
       def die
-        @reactor.log(:info, "#{self.class}, Worker [#{@identity}] is exiting.")
-        @heartbeat_timer.cancel
+        @reactor.log(:info, "#{self.class}, Worker [#{@identity}] for service [#{@service_name}] is exiting.")
+        canceled = @heartbeat_timer.cancel
+
+        unless canceled
+          @reactor.log(:error, "#{self.class}, Worker [#{@identity}] for service [#{@service_name}] could *not* cancel heartbeat timer on reactor [#{@reactor.name}]. There must be a threading problem somewhere. Make sure this object is *only accessed* from its reactor thread!")
+        end
+
+        @dead = true
       end
 
       # True when this worker reference hasn't received a heartbeat from its worker
@@ -56,13 +78,13 @@ module RzmqBrokers
         elapsed = ((Time.now - @hb_received_at) * 1_000)
 
         if elapsed > (@heartbeat_interval * @heartbeat_retries)
-          @reactor.log(:warn, "#{self.class}, Broker Worker [#{@identity}] expiring, last received hb at #{@hb_received_at}")
+          @reactor.log(:warn, "#{self.class}, Broker Worker [#{@identity}] for service [#{@service_name}] is expiring, last received hb at #{@hb_received_at}")
           true
         else
           false
         end
       end
-    end
+    end # Worker
 
   end
 end
